8d01c1f90c687a7ed22f02dcdea648650376aa49
CWIN77 <cwstbp0426@gmail.com>
2023-01-08 23:31:14 +0900

    gorm 코드 작성

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	middleware "psql/Tools/middleware"
 
@@ -13,6 +14,13 @@ import (
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
+
+type User struct {
+	ID        uint `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Name      string
+}
 
 func main() {
 	if err := godotenv.Load(".env"); err != nil {
@@ -26,17 +34,19 @@ func main() {
 	middleware.Setting(app)
 	var (
 		host     = os.Getenv("HOST")
-		port     = 5432
 		user     = os.Getenv("USER")
 		password = os.Getenv("PASSWORD")
 		dbname   = os.Getenv("DBNAME")
 	)
-	dbinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	dbinfo := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable", host, user, password, dbname)
+	// db, err := gorm.Open(postgres.Open(dbinfo), &gorm.Config{})
+	// if err != nil {
+	// 	panic(err)
+	// }
 	db, err := sql.Open("postgres", dbinfo)
 	if err != nil {
 		panic(err)
 	}
-
 	defer db.Close()
 
 	err = db.Ping()
@@ -46,7 +56,24 @@ func main() {
 
 	app.Get("/", func(c *fiber.Ctx) error {
 
-		return c.Status(200).JSON("Hello World!")
+		// * 테이블 생성
+		// db.Migrator().CreateTable(&User{})
+
+		// * 데이터 생성
+		// user := User{Name: "CWIN77"}
+		// result := db.Create(&user)
+		// if result.Error != nil {
+		// 	panic(result.Error)
+		// }
+		// db.Select("Name", "ID", "CreatedAt","UpdatedAt","DeletedAt").Create(&user)
+
+		// * 데이터 가져오기
+		// var result User
+		// db.Model(User{}).First(&result)
+		// fmt.Println(result)
+
+		getData(db)
+		return c.Status(200).JSON("*result")
 	})
 
 	app.Listen(":5000")
@@ -55,8 +82,8 @@ func main() {
 func createTable(db *sql.DB) {
 	statement, err := db.Prepare(
 		`CREATE TABLE IF NOT EXISTS test (
-			id        INT PRIMARY KEY,
-			name      TEXT
+			id   SERIAL NOT NULL PRIMARY KEY,
+			name TEXT
 		)`)
 	if err != nil {
 		panic(err)
@@ -68,7 +95,7 @@ func createTable(db *sql.DB) {
 }
 
 func dropTable(db *sql.DB) {
-	statement, err := db.Prepare(`DROP TABLE test`)
+	statement, err := db.Prepare(`DROP TABLE users`)
 	if err != nil {
 		panic(err)
 	}
@@ -79,24 +106,34 @@ func dropTable(db *sql.DB) {
 }
 
 func getData(db *sql.DB) {
-	rows, err := db.Query(`SELECT "name", "id" FROM "test"`)
+	rows, err := db.Query(`SELECT tablename FROM pg_catalog.pg_tables`)
 	if err != nil {
 		panic(err)
 	}
 
 	defer rows.Close()
 	for rows.Next() {
-		var name string
-		var id int
+		// var name string
+		// var id int
 
-		err = rows.Scan(&name, &id)
+		var data interface{}
+		err = rows.Scan(&data)
 		if err != nil {
 			panic(err)
 		}
 
-		fmt.Println(name, id)
+		fmt.Printf("%s", data)
 	}
 
+	if err != nil {
+		panic(err)
+	}
+}
+
+func addData(db *sql.DB) {
+	insertDynStmt := `insert into "users"("name") values($1)`
+	// * insert into users ("name" ,created_at,updated_at) values ('cwub7777',NOW(),NOW())
+	_, err := db.Exec(insertDynStmt, "CWIN")
 	if err != nil {
 		panic(err)
 	}
